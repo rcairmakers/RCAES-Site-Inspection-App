b58b440369b4ae213e27df27c699542cfe720283
rcairmakers <r.mondejar.rcaes@gmail.com>
2026-09-05 16:15:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycby62b9OF_EOjvs9SqpmBdNLFXIccpNv5ERX0h_qf16yGq-ub-RcgTMyjREHwytkwt0m6Q/exec;
+const API_URL = 'https://script.google.com/macros/s/AKfycbxasKYifvKdXkHcLw39gar8K2rzS2TQHFwSSNG3HV-k7qECabcwCudloYaBB8EMJmfd4g/exec';
 const SESSION_KEY='rcaes_session_v1';
 const QUEUE_KEY='rcaes_queue_v1';
 let session=null, step=0;
